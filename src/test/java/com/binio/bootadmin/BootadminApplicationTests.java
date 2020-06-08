f3585dc53edf60e49afa4aba1576c7e99f116b44
binio <tbiniecki@gmail.com>
2020-06-08 15:11:55 +0100

    add files

---
+++ src/test/java/com/binio/bootadmin/BootadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binio.bootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
